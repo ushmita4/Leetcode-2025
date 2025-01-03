ca0c51bd50c1c7dcb0189040fc42b713b6ccceb5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2025-01-03 22:29:26 +0530

    Create numberofwaystosplitarray.cpp

---
+++ 01. January/numberofwaystosplitarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    #define ll long long
+    int waysToSplitArray(vector<int>& nums) {
+        int n = nums.size();
+        ll ans = 0;
+
+    
+        vector<ll> pref(n, 0);
+        pref[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            pref[i] = nums[i] + pref[i - 1];
+        }
+
+ 
+        ll totalSum = pref[n - 1];
+
+     
+        for (int i = 0; i < n - 1; i++) {
+            if (pref[i] >= (totalSum - pref[i])) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
