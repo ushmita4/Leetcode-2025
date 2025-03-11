b23e25b4f71a1bb021498f2e884729f386e6880e
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2025-03-11 21:44:23 +0530

    Create numberofsubstringscontainingallthreecharacters.cpp

---
+++ 03. March/numberofsubstringscontainingallthreecharacters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int len = s.length();
+        int left = 0, right = 0;
+      
+        vector<int> freq(3, 0);
+        int total = 0;
+
+        while (right < len) {
+         
+            char curr = s[right];
+            freq[curr - 'a']++;
+
+      
+            while (hasAllChars(freq)) {
+              
+                total += len - right;
+
+         
+                char leftChar = s[left];
+                freq[leftChar - 'a']--;
+                left++;
+            }
+
+            right++;
+        }
+
+        return total;
+    }
+
+private:
+    bool hasAllChars(vector<int>& freq) {
+    
+        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
+    }
+};
