951bc87f824a8fa85c32e68aa3df7d5d8b60b0e5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2025-04-07 22:14:58 +0530

    Create partitionequalsubsetsum.cpp

---
+++ 04. April/partitionequalsubsetsum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int sumOf(const vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        return sum;
+    }
+
+    bool rec(int i, const vector<int>& nums, int n, int sum, vector<vector<int>>& dp) {
+        if (sum == 0) return true;
+        if (sum < 0 || i >= n) return false;
+        if (dp[i][sum] != -1) return dp[i][sum] == 1;
+
+        bool dont = rec(i + 1, nums, n, sum, dp);
+        bool pick = (nums[i] <= sum) ? rec(i + 1, nums, n, sum - nums[i], dp) : false;
+        
+        dp[i][sum] = (pick || dont) ? 1 : 0;
+        return pick || dont;
+    }
+
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = sumOf(nums);
+        if (sum % 2 == 1) return false;
+        sum /= 2;
+        
+        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
+        return rec(0, nums, n, sum, dp);
+    }
+};
